3c4f9858fe9a39db1157238c00a5743a33b583af
joetutan <noobianjoe@outlook.com>
2025-12-22 09:36:16 +0200

    Initialise core functions

---
+++ app_/core/core_functions.py
@@ -0,0 +1,17 @@
+
+Expenses_data = {}
+
+def add_exp(id:int, description:str, amount: int, category: str) -> None:
+    print()
+
+def update_exp(id:int, description:str, amount: int, category: str) -> None:
+    print()
+
+def delete_exp(id:str)->None:
+    print()
+
+def list_exp(filter:str)->None:
+    print()
+
+def set_budget(budget:int)->None:
+    print()
