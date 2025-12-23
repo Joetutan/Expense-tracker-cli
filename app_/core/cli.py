f83db21fda461f07ae990ae6e048533aa337e466
joetutan <noobianjoe@outlook.com>
2025-12-23 10:18:13 +0200

    cli argument parser and add_exp function

--- app_/core/cli.py
+++ app_/core/cli.py
@@ -1,2 +1,21 @@
+import argparse
+from app_.core.core_functions import add_exp
+
+
 def main() ->None:
-    print("")
+    
+    parser = argparse.ArgumentParser(description="A simple cli expense tracker")
+
+    subparsers = parser.add_subparsers(dest="command")
+
+    # ---add expense---
+    add_parser = subparsers.add_parser("add", help="add new expense")
+    add_parser.add_argument("category", type=str,help="expense category")
+    add_parser.add_argument("description", type=str , help="expense description")
+    add_parser.add_argument("amount", type=int, help="amount spent")
+
+    args = parser.parse_args()
+
+    match args.command:
+        case "add":
+            add_exp(args.category, args.description, args.amount)
--- app_/core/core_functions.py
+++ app_/core/core_functions.py
@@ -1,8 +1,14 @@
-
+from datetime import datetime
 Expenses_data = {}
 
-def add_exp(id:int, description:str, amount: int, category: str) -> None:
-    print()
+
+def add_exp(category: str,  description:str, amount: int ) -> None:
+    #---generate unique ID---
+    task_id = len(Expenses_data)+1
+    #---genearte date item---
+    time_stamp = f'{datetime.now().strftime("%Y-%m-%d")}'
+    print("ID.  Date          Category    description                   amount")
+    print(f"{task_id}.    {time_stamp}    {category}         {description}                  ${amount}")
 
 def update_exp(id:int, description:str, amount: int, category: str) -> None:
     print()
