967643d4b474a6e6b59c4595bd28952018d6f74a
joetutan <noobianjoe@outlook.com>
2025-12-25 15:12:31 +0200

    update add function and list function

--- app_/core/cli.py
+++ app_/core/cli.py
@@ -1,5 +1,5 @@
 import argparse
-from app_.core.core_functions import add_exp
+from app_.core.core_functions import add_exp,list_exp
 
 
 def main() ->None:
@@ -14,8 +14,16 @@ def main() ->None:
     add_parser.add_argument("description", type=str , help="expense description")
     add_parser.add_argument("amount", type=int, help="amount spent")
 
+    # --- list expense ---
+    list_parser = subparsers.add_parser("list", argument_default="all", help="list expenses [month, date]")
+    list_parser.add_argument("-m","--month",default=None, type=int, choices=range(1, 13), metavar="MM",  help="Filter by month (1–12)")
+    list_parser.add_argument("-d","--date", default=None, type=int, choices=range(1, 32), metavar="DD",  help="Filter by month (1–31)")
+
+
     args = parser.parse_args()
 
     match args.command:
         case "add":
             add_exp(args.category, args.description, args.amount)
+        case "list":
+            list_exp(args.month, args.date)
